c7bb5aeba9f5d2a2ba767b6dcd753e2e478e3995
Md. Delwar Hossain <delwarbscse@gmail.com>
2024-12-04 04:03:38 +0600

    Add animation by AOS in banner image

--- src/components/banner/RightBanner.jsx
+++ src/components/banner/RightBanner.jsx
@@ -12,13 +12,14 @@ const RightBanner = () => {
       delay: 500
     })
   },[]);
+  
 
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center">
       <div data-aos="zoom-in" data-aos-delay="500"  className='relative w-[80%] flex justify-center items-center '>
-          <img src={design} alt="Design image" className='w-[65%] animate-pulse pb-3 pr-3' />
+          <img src={design} alt="Design image" className='w-[65%] pb-3 pr-3 animate-pulse' />
 
-          <img src={circleShape} alt="circle shape" className='absolute w-[90%] lgl:rotateAnimation'/>
+          <img src={circleShape} alt="shape" data-aos="fade-up" data-aos-delay="750" data-aos-duration="500" data-aos-once="true" loading="lazy" width="524" height="518" decoding="async" data-nimg="1" className='absolute w-[90%] rotateAnimation aos-init aos-animate'/>
         </div>
     </div>
   );
