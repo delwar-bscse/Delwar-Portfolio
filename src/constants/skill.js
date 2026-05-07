b34a88861c24895053195939af09e3f113cf148e
Md. Delwar Hossain <delwarbscse@gmail.com>
2026-05-07 16:47:53 +0600

    Update skills

--- src/constants/skill.js
+++ src/constants/skill.js
@@ -2,27 +2,37 @@
 export const designSkill = [
   {
     name:"Tailwind CSS",
-    skillwidth:"w-[65%]",
-    width:"65"
+    skillwidth:"w-[85%]",
+    width:"85"
   },
   {
     name:"React JS",
     skillwidth:"w-[90%]",
+    width:"90"
+  },
+  {
+    name:"Next JS",
+    skillwidth:"w-[80%]",
     width:"80"
   },
   {
     name:"Node JS",
-    skillwidth:"w-[50%]",
-    width:"50"
+    skillwidth:"w-[80%]",
+    width:"80"
   },
   {
     name:"Express JS",
-    skillwidth:"w-[40%]",
-    width:"40"
+    skillwidth:"w-[85%]",
+    width:"85"
   },
   {
     name:"Mongo DB",
     skillwidth:"w-[70%]",
     width:"70"
+  },
+  {
+    name:"Nest JS",
+    skillwidth:"w-[50%]",
+    width:"50"
   }
 ]
